07d566eb11c60f1f4bed3d5ad7b884360e57df59
Omar <omarhonor1@gmail.com>
2026-04-15 21:21:23 +0300

    Create initial version of MiniPetSimulator

---
+++ main.py
@@ -0,0 +1,6 @@
+from pet import Pet
+
+pet = Pet("Milo", 50)
+
+print("Pet name:", pet.name)
+print("Energy level:", pet.energy_level)
---
+++ pet.py
@@ -0,0 +1,4 @@
+class Pet:
+    def __init__(self, name, energy_level):
+        self.name = name
+        self.energy_level = energy_level
