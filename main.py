873a90761ab3d27269023ced14ed68ef63869dfd
Omar <omarhonor1@gmail.com>
2026-04-15 22:17:46 +0300

    Add play_with_pet feature

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ pet = Pet("Milo", 50)
 
 print("Pet name:", pet.name)
 print("Energy level:", pet.energy_level)
+
+pet.play_with_pet()
+print("Energy level after playing:", pet.energy_level)
--- pet.py
+++ pet.py
@@ -2,3 +2,7 @@ class Pet:
     def __init__(self, name, energy_level):
         self.name = name
         self.energy_level = energy_level
+
+    def play_with_pet(self):
+        self.energy_level -= 10
+        print(f"{self.name} played and lost some energy.")
